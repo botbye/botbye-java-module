17da63300eaa89d28b1a667843e66e1240a38da9
botbye <accounts@botbye.com>
2024-06-28 19:51:37 +0300

    feat: support extra data

---
+++ src/main/java/com.botbye/model/BotbyeExtraData.java
@@ -0,0 +1,176 @@
+package com.botbye.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BotbyeExtraData implements Serializable {
+    /**
+     * The real IP address of the user. If a proxy is used, this will be the real IP address, otherwise, it will be the same as `ip`.
+     * Note: This does not apply to VPN usage.
+     */
+    private final String realIp = null;
+    private final String realCountry = null;
+    /**
+     * The IP address of the user. If a proxy is used, this will be the proxy IP address.
+     */
+    private String ip = null;
+    private String asn = null;
+    private String country = null;
+    private String browser = null;
+    private String browserVersion = null;
+    private String deviceName = null; // Galaxy S9
+    private String deviceType = null; // Mobile Phone
+    private String deviceCodeName = null; // SM-G960F
+    private String platform = null; // Android
+    private String platformVersion = null; // 8
+
+    public BotbyeExtraData() {
+    }
+
+    public BotbyeExtraData(
+        String ip,
+        String asn,
+        String country,
+        String browser,
+        String browserVersion,
+        String deviceName,
+        String deviceType,
+        String deviceCodeName,
+        String platform,
+        String platformVersion
+    ) {
+        this.ip = ip;
+        this.asn = asn;
+        this.country = country;
+        this.browser = browser;
+        this.browserVersion = browserVersion;
+        this.deviceName = deviceName;
+        this.deviceType = deviceType;
+        this.deviceCodeName = deviceCodeName;
+        this.platform = platform;
+        this.platformVersion = platformVersion;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getAsn() {
+        return asn;
+    }
+
+    public void setAsn(String asn) {
+        this.asn = asn;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getBrowser() {
+        return browser;
+    }
+
+    public void setBrowser(String browser) {
+        this.browser = browser;
+    }
+
+    public String getBrowserVersion() {
+        return browserVersion;
+    }
+
+    public void setBrowserVersion(String browserVersion) {
+        this.browserVersion = browserVersion;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public String getDeviceCodeName() {
+        return deviceCodeName;
+    }
+
+    public void setDeviceCodeName(String deviceCodeName) {
+        this.deviceCodeName = deviceCodeName;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getPlatformVersion() {
+        return platformVersion;
+    }
+
+    public void setPlatformVersion(String platformVersion) {
+        this.platformVersion = platformVersion;
+    }
+
+    public String getRealIp() {
+        return realIp;
+    }
+
+    public String getRealCountry() {
+        return realCountry;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BotbyeExtraData that)) return false;
+        return Objects.equals(realIp, that.realIp) && Objects.equals(realCountry, that.realCountry) && Objects.equals(ip, that.ip) && Objects.equals(asn, that.asn) && Objects.equals(country, that.country) && Objects.equals(browser, that.browser) && Objects.equals(browserVersion, that.browserVersion) && Objects.equals(deviceName, that.deviceName) && Objects.equals(deviceType, that.deviceType) && Objects.equals(deviceCodeName, that.deviceCodeName) && Objects.equals(platform, that.platform) && Objects.equals(platformVersion, that.platformVersion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(realIp, realCountry, ip, asn, country, browser, browserVersion, deviceName, deviceType, deviceCodeName, platform, platformVersion);
+    }
+
+    @Override
+    public String toString() {
+        return "BotbyeExtraData{" +
+            "realIp='" + realIp + '\'' +
+            ", realCountry='" + realCountry + '\'' +
+            ", ip='" + ip + '\'' +
+            ", asn='" + asn + '\'' +
+            ", country='" + country + '\'' +
+            ", browser='" + browser + '\'' +
+            ", browserVersion='" + browserVersion + '\'' +
+            ", deviceName='" + deviceName + '\'' +
+            ", deviceType='" + deviceType + '\'' +
+            ", deviceCodeName='" + deviceCodeName + '\'' +
+            ", platform='" + platform + '\'' +
+            ", platformVersion='" + platformVersion + '\'' +
+            '}';
+    }
+}
--- src/main/java/com.botbye/model/BotbyeResponse.java
+++ src/main/java/com.botbye/model/BotbyeResponse.java
@@ -1,30 +1,47 @@
 package com.botbye.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.io.Serializable;
 import java.util.Objects;
 import java.util.UUID;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BotbyeResponse implements Serializable {
     private BotbyeChallengeResult result;
     private UUID reqId;
     private BotbyeError error;
+    private BotbyeExtraData extraData;
 
     public BotbyeResponse() {
         this.result = new BotbyeChallengeResult();
         this.reqId = UUID.fromString("00000000-0000-0000-0000-000000000000");
         this.error = null;
+        this.extraData = null;
     }
 
     public BotbyeResponse(BotbyeError error) {
         this.result = new BotbyeChallengeResult();
         this.reqId = UUID.fromString("00000000-0000-0000-0000-000000000000");
         this.error = error;
+        this.extraData = null;
     }
 
-    public BotbyeResponse(BotbyeChallengeResult result, UUID reqId, BotbyeError error) {
+    public BotbyeResponse(BotbyeChallengeResult result, UUID reqId, BotbyeError error, BotbyeExtraData extraData) {
         this.result = result;
         this.reqId = reqId;
         this.error = error;
+        this.extraData = extraData;
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    public BotbyeExtraData getExtraData() {
+        return extraData;
+    }
+
+    public void setExtraData(BotbyeExtraData extraData) {
+        this.extraData = extraData;
     }
 
     public BotbyeChallengeResult getResult() {
@@ -54,14 +71,13 @@ public class BotbyeResponse implements Serializable {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BotbyeResponse that = (BotbyeResponse) o;
-        return Objects.equals(result, that.result) && Objects.equals(reqId, that.reqId) && Objects.equals(error, that.error);
+        if (!(o instanceof BotbyeResponse that)) return false;
+        return Objects.equals(result, that.result) && Objects.equals(reqId, that.reqId) && Objects.equals(error, that.error) && Objects.equals(extraData, that.extraData);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(result, reqId, error);
+        return Objects.hash(result, reqId, error, extraData);
     }
 
     @Override
@@ -70,6 +86,7 @@ public class BotbyeResponse implements Serializable {
                 "result=" + result +
                 ", reqId=" + reqId +
                 ", error=" + error +
+                ", extraData=" + extraData +
                 '}';
     }
 }
